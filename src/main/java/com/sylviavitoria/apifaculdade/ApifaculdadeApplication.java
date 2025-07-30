6e75c83c248d9137c0d17857f65c1d0380ed55e7
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-30 16:35:07 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/sylviavitoria/apifaculdade/ApifaculdadeApplication.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.apifaculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifaculdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sylviavitoria/apifaculdade/ApifaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.apifaculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
