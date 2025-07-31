ad4654c15f336781b45f00605f2c8f11f287194d
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-31 15:41:13 -0300

    feat: adicionar classe Professor e relacionamento com Usuario

--- src/main/java/com/sylviavitoria/apifaculdade/model/Professor.java
+++ src/main/java/com/sylviavitoria/apifaculdade/model/Professor.java
@@ -1,18 +1,11 @@
 package com.sylviavitoria.apifaculdade.model;
 
-import com.sylviavitoria.apifaculdade.enums.TipoUsuario;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,30 +16,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "tb_usuario")
-public class Usuario {
-
+@Table(name = "tb_professor")
+public class Professor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // @Email
+    @Column(nullable = false)
+    private String nome;
+
     @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String senha;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private TipoUsuario tipo; 
-
-    @OneToOne
-    @JoinColumn(name = "aluno_id", referencedColumnName = "id")
-    private Aluno aluno;
-
-    @OneToOne
-    @JoinColumn(name = "professor_id", referencedColumnName = "id")
-    private Professor professor;
 }
-
