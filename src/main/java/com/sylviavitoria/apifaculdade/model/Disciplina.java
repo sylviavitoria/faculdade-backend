12d442586bc865713e7b39eaea5df6409086174d
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-04 10:15:10 -0300

    feat: criar entidade Disciplina

---
+++ src/main/java/com/sylviavitoria/apifaculdade/model/Disciplina.java
@@ -0,0 +1,38 @@
+package com.sylviavitoria.apifaculdade.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_disciplina")
+public class Disciplina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String codigo;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
+    
+}
