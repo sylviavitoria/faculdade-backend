f76c8a20c120901516935985b5403cd6ebf3c4a1
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-04 16:43:38 -0300

    feat: adicionar configuração do Swagger para documentação da API

---
+++ src/main/java/com/sylviavitoria/apifaculdade/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.sylviavitoria.apifaculdade.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Sistema Faculdade")
+                        .version("1.0.0")
+                        .description("API para gerenciamento do sistema acadêmico")
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
